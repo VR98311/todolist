b07159673c8a317d3f16312c4301392c776e9d11
VR98311 <vikyathrao311@gmail.com>
2023-05-26 12:10:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+
+ var items = [];
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
+app.get("/",(req,res) =>{
+
+  var today = new Date(); 
+
+  var options = {
+    weekday:"long",
+    day:"numeric",
+    month:"long",
+  }
+
+  var day = today.toLocaleDateString("en-US",options);
+
+  res.render("list",{kindofday:day,listitems:items});
+});
+
+app.post("/",(req,res)=>{
+  var itemToAdd =  req.body.item;
+   items.push(itemToAdd);
+  res.redirect("/");
+});
+
+
+
+app.listen(3000,(req,res) =>{
+  console.log("U r connected to 3000 port");
+});
